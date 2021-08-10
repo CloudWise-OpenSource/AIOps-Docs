d6ed21b568e903f0e756757f98440e7a8701e43f
biukang.niu <biukang.niu@cloudwise.com>
2021-08-10 11:05:54 +0800

    update

--- docs/.vuepress/config/locales/zh/sidebar.js
+++ docs/.vuepress/config/locales/zh/sidebar.js
@@ -17,34 +17,23 @@ module.exports = {
       collapsable: false,
       children: [
         '',
-        'code-of-conduct',
+        'introduction',
         'releases',
-        'contributing',
-        'bugs',
-        'faq'
       ]
     },
     {
-      title: '部署',
+      title: '基本使用',
       collapsable: false,
       children: [
-        'install',
-        'update',
-        'troubleshoot'
-      ]
-    },
-    {
-      title: '管理',
-      collapsable: false,
-      children: [
-        ['admin','后台管理'],
-        'config',
-        'extensions',
-        'languages',
-        'themes',
-        ['mail','配置邮箱'],
-        'console'
+        'file_manage',
+        'develop_env',
+        'service_manage',
+        'resource_manage',
+        'service_manage',
+        'system_manage',
+        'competence'
       ]
     }
+    
   ]
 }

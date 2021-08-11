14c41d1485b24b03a54f26998424a7e2f7600956
biukang.niu <biukang.niu@cloudwise.com>
2021-08-11 18:32:20 +0800

    更新文档

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
 # Docs文档使用方法
 
+***
+
+CICD运行状态
+
 [![pipeline status](https://git.cloudwise.com/rdep/docs/badges/master/pipeline.svg)](https://git.cloudwise.com/rdep/docs/-/commits/master)
 
-文档目录结构
+***
 
+## 文档目录结构
+```
+
+```
+
+## 开始写作
+
+静态资源位置（图片）
+
+## 修订目录结构
 
--- docs/.vuepress/config/locales/zh/sidebar.js
+++ docs/.vuepress/config/locales/zh/sidebar.js
@@ -33,7 +33,17 @@ module.exports = {
         'system_manage',
         'competence'
       ]
+    },
+    {
+      title: '算法服务',
+      collapsable: false,
+      children: [
+        '基于自动阈值的异常检测',
+        '同环比异常检测算法',
+        '固定阈值异常检测算法',
+        '基于标准差异常检测算法',
+        '基于频域分析的异常检测'
+      ]
     }
-    
   ]
 }

b832e428519392c7c320b3533111eea71b6efc91
biukang.niu <biukang.niu@cloudwise.com>
2021-08-24 10:45:39 +0800

    update

--- docs/.vuepress/config/locales/zh/sidebar.js
+++ docs/.vuepress/config/locales/zh/sidebar.js
@@ -65,25 +65,10 @@ module.exports = {
       collapsable: false,
       children: [
         'getting_started',
-        'file_manage',
-        'develop_env',
-        'service_manage',
-        'resource_manage',
-        'service_manage',
-        'system_manage',
-        'competence'
-      ]
-    },
-    {
-      title: '算法服务',
-      collapsable: false,
-      children: [
-        '基于自动阈值的异常检测',
-        '同环比异常检测算法',
-        '固定阈值异常检测算法',
-        '基于标准差异常检测算法',
-        '基于频域分析的异常检测',
-        '线性回归预测算法文档'
+        'manual-file-manger',
+        'manual-dev-env',
+        'manual-image-manger',
+        'manual-service-manger'
       ]
     }
   ],
